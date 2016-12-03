01c2541ef43d324406980a5096997382bfebe0eb
ngattusohw <ngattusohw@gmail.com>
2016-12-03 18:25:03 -0500

    Added "Receive newsletter emails"
    Added functionality to the newsletter checkbox in contact.html, and
    handled it in formResp.js

--- formResp.js
+++ formResp.js
@@ -4,10 +4,23 @@
 function callBack(){
 
 
-
 	var x = document.getElementById("form");
     var name = x.elements[0].value;
-    var text = "You will be emailed at " + x.elements[1].value + " and you will recieve newsletter emails!";
+
+    //testing regex.. will implement later
+	// var email = new RegExp("/\w@\w.com | .net | .org");
+	// var str = x.elements[1].value;
+	// console.log(email.test(str));
+	var text;
+
+	console.log(x.elements[6].checked);
+
+	if(x.elements[6].checked){
+		text = "You will be emailed at " + x.elements[1].value + " and you will recieve newsletter emails! :)";
+	}else{
+		text = "You will be emailed at " + x.elements[1].value + " and you will NOT recieve newsletter emails! :(";
+	}
+    
 
     document.getElementById("replacer").innerHTML = "<h2 class='csstext'>Thank you " + name + " for your feedback!</h2>";
     document.getElementById("placer").innerHTML = text;
