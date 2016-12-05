a1bbbba07fb7b0e4882324836a2ed907902f25a3
ngattusohw <ngattusohw@gmail.com>
2016-12-04 19:03:26 -0500

    Adding valid email requirement
    changed formResp.js to require all fields, and a valid email address.

--- formResp.js
+++ formResp.js
@@ -5,20 +5,31 @@ function callBack(){
 
 
 	var x = document.getElementById("form");
-    var name = x.elements[0].value;
 
-	var r = /\w+@\w+\.(com|net|org)/;
-	var str = x.elements[1].value;
-	console.log(r.test(str));
-
-	var text;
-	if(x.elements[6].checked){
-		text = "You will be emailed at " + x.elements[1].value + " and you will receive newsletter emails! :)";
+	if(x.elements[0].value == "" || x.elements[1].value == "" || x.elements[7].value== ""){
+		alert("Please fill out all field before submitting!");
 	}else{
-		text = "You will be emailed at " + x.elements[1].value + " and you will NOT receive newsletter emails! :(";
-	}
-    
+		
 
-    document.getElementById("replacer").innerHTML = "<h2 class='csstext'>Thank you " + name + " for your feedback!</h2>";
-    document.getElementById("placer").innerHTML = text;
+		var r = /\w+@\w+\.(com|net|org)/;
+		var str = x.elements[1].value;
+		if(r.test(str)){
+			var text;
+			if(x.elements[6].checked){
+				text = "You will be emailed at " + x.elements[1].value + " and you will receive newsletter emails! :)";
+			}else{
+				text = "You will be emailed at " + x.elements[1].value + " and you will NOT receive newsletter emails! :(";
+			}
+		    
+			var name = x.elements[0].value;
+		    document.getElementById("replacer").innerHTML = "<h2 class='csstext'>Thank you " + name + " for your feedback!</h2>";
+		    document.getElementById("placer").innerHTML = text;
+		}else{
+			alert("Please enter a valid email address!");
+		}
+
+		
+	}
+
+    
 }
